a3311e7ca464b4e4f9ddcffa7084726e0dd574b4
suzhi82 <suzhi82@gmail.com>
2019-05-01 11:30:39 +0930

    reconstruct route1

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const { check, validationResult } = require('express-validator/check');
 
 
 mongoose.connect("mongodb+srv://abc01:abc01@clustersgp-nsn0q.mongodb.net/nodejs-blog?retryWrites=true", { useNewUrlParser: true });
@@ -35,96 +34,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let Article = require('./models/article');
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+let articles = require('./routes/articles');
+
+app.use('/articles', articles);
+
+let Article = require('./models/article');
 app.get('/', function(req, res) {
   Article.find({}, function(err, articles) {
-    res.render('index', {
+    res.render('articles/index', {
       articles: articles
     });
   });
 });
 
-app.get('/articles/new', function(req, res) {
-  res.render('new', {
-    title: 'Add Article'
-  });
-});
-
-app.get('/articles/:id', function(req, res) {
-  Article.findById(req.params.id, function(err, article) {
-    res.render('show', {
-      article: article
-    });
-  });
-});
-
-app.get('/articles/:id/edit', function(req, res) {
-  Article.findById(req.params.id, function(err, article) {
-    res.render('edit', {
-      title: 'Edit Article',
-      article: article
-    });
-  });
-});
-
-app.post(
-  '/articles/create', [
-    check('title').isLength({ min: 1 }).withMessage('Title is required'),
-    check('author').isLength({ min: 1 }).withMessage('Author is required'),
-    check('body').isLength({ min: 1 }).withMessage('Body is required')
-  ], function(req, res) {
-    const errors = validationResult(req);
-
-    if(!errors.isEmpty()) {
-      res.render('new', {
-        title: 'Add Article',
-        errors: errors.array()
-      });
-    } else {
-      let article = new Article(req.body);
-
-      article.save(function(err) {
-        if(err) {
-          console.log(err);
-          return;
-        } else {
-          req.flash("success", "Article Added");
-          res.redirect('/');
-        }
-      });
-    }
-  }
-);
-
-app.post('/articles/update/:id', function(req, res) {
-  let query = { _id: req.params.id };
-
-  Article.update(query, req.body, function(err, article) {
-    if(err) {
-      console.log(err);
-      return;
-    } else {
-      req.flash("success", "Article Updated");
-      res.redirect('/');
-    }
-  });
-});
-
-app.delete('/articles/:id', function(req, res) {
-  let query = { _id: req.params.id };
-
-  Article.remove(query, function(err) {
-    if(err) {
-      console.log(err);
-    }
-
-    res.send('Success');
-  });
-});
 
 app.listen(5000, function() {
   console.log("Server started on port 5000...");
---
+++ routes/articles.js
@@ -0,0 +1,94 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator/check');
+
+let router = express.Router();
+let Article = require('../models/article');
+
+router.get('/', function(req, res) {
+  Article.find({}, function(err, articles) {
+    res.render('articles/index', {
+      articles: articles
+    });
+  });
+});
+
+router.get('/new', function(req, res) {
+  res.render('articles/new', {
+    title: 'Add Article'
+  });
+});
+
+router.get('/:id', function(req, res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('articles/show', {
+      article: article
+    });
+  });
+});
+
+router.get('/:id/edit', function(req, res) {
+  Article.findById(req.params.id, function(err, article) {
+    res.render('articles/edit', {
+      title: 'Edit Article',
+      article: article
+    });
+  });
+});
+
+router.post(
+  '/create', [
+    check('title').isLength({ min: 1 }).withMessage('Title is required'),
+    check('author').isLength({ min: 1 }).withMessage('Author is required'),
+    check('body').isLength({ min: 1 }).withMessage('Body is required')
+  ], function(req, res) {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      res.render('articles/new', {
+        title: 'Add Article',
+        errors: errors.array()
+      });
+    } else {
+      let article = new Article(req.body);
+
+      article.save(function(err) {
+        if(err) {
+          console.log(err);
+          return;
+        } else {
+          req.flash("success", "Article Added");
+          res.redirect('/');
+        }
+      });
+    }
+  }
+);
+
+router.post('/update/:id', function(req, res) {
+  let query = { _id: req.params.id };
+
+  Article.updateOne(query, req.body, function(err, article) {
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      req.flash("success", "Article Updated");
+      res.redirect('/');
+    }
+  });
+});
+
+router.delete('/:id', function(req, res) {
+  let query = { _id: req.params.id };
+
+  Article.deleteOne(query, function(err) {
+    if(err) {
+      console.log(err);
+    }
+
+    req.flash("success", "Article Deleted");
+    res.send('Success');
+  });
+});
+
+module.exports = router;
