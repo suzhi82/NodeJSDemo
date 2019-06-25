7a7089ccfeb068551ad2d9fc01a08ad2a38cfdc4
root <root@vultr.guest>
2019-06-25 13:43:19 +0800

    changed app.js port to 8000

--- app.js
+++ app.js
@@ -64,9 +64,9 @@ app.get('/', function(req, res) {
   });
 });
 
-
-app.listen(5000, function() {
-  console.log("Server started on port 5000...");
+let port = 8000;
+app.listen(port, function() {
+  console.log("Server started on port " +  port + " ...");
 });
 
 
